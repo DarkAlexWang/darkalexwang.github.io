dbe5c0092c602e73705d78e616f88024431830ea
Alex Wang <wangzhihuan0815@gmail.com>
2022-01-30 22:18:55 -0800

    Site updated: 2022-01-30 22:18:55

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/about/index.html","40812e875e80ca97b3633cdeb9aecee7"]];
+var precacheConfig = [["/about/index.html","d41d8cd98f00b204e9800998ecf8427e"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
